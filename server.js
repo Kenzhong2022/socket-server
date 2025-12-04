56da2f878f9b8ded4bc9745ee713885c543d4bbc
kenZhongKai <997260348@qq.com>
2025-12-04 11:54:48 +0800

    修复奔溃问题，原因是回调函数没写

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const io = new Server(httpServer, {
 
 // 监听客户端连接事件
 io.on("connection", async (socket) => {
-  socket.on("connection", "连接成功");
+  socket.on("connection", () => {
+    console.log("连接成功");
+  });
   console.log(`客户端连接: ${socket.id}`);
   // 向客户端发送连接成功的问候
   socket.emit("hello", "来自服务器【本地socket-server】的问候");
